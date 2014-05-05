79a2996401759e1182b4520236eaee0303785a78
Tai Wei Guo <tg225@cornell.edu>
2014-05-04 21:10:57 -0400

    small get functions for player

--- player.ml
+++ player.ml
@@ -11,6 +11,10 @@ let color_of (c,h,t) : color = c
 let inv_of (c, (i,cds), t) : inventory = i
 let cards_of (c, (i,cds), t) : cards = cds
 let trophs_of (c, h, t) : trophies = t
+let num_knights p : int = let (k,_,_) = trophs_of p in k
+let has_longest_road p : bool = let (_,longest,_) = trophs_of p in longest
+let has_largest_army p : bool = let (_,_largest) = trophs_of p in _largest
+
 
 (* player c pl return the player corresponding to color c in pl *)
 let player (c : color) (pl : player list) : player =

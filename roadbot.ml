707f98f316715833200e270e27f9201a88eeb4b0
taenin <persons.matthew@gmail.com>
2014-05-06 20:21:53 +0000

    Roadbot now tries to move toward intersection 53 as efficiently as possible

--- roadbot.ml
+++ roadbot.ml
@@ -40,10 +40,30 @@ module Bot = functor (S : Soul) -> struct
     let (c, r) = n in
     reset_turn:= (if !reset_turn = !turn_count then !reset_turn
                   else ((new_turn()); !reset_turn+1));
+
+    let myroads = roads_of c rl in
+    let paths= if List.length myroads = 0 then [] 
+        else List.fold_left (fun acc (_, (pt1, pt2)) -> 
+        let path1 = shortest_path_to 53 c pl [(pt1, None)] [] [] rl il in
+        let path2 = shortest_path_to 53 c pl [(pt2, None)] [] [] rl il in
+        path1::(path2::acc)) [] myroads in
+        
+    let pathlengths = List.map (List.length) paths in
+    let ((_,chosenPathInd),_) = if List.length paths = 0 then ((0, -1), -1)
+                                else List.fold_left (fun ((minpath, pathindex), index) x -> if x <=minpath && x <> 0 then ((x, index), index+1) 
+                                        else ((minpath, pathindex), index+1)) ((cNUM_POINTS, -1),0) pathlengths in
+    let chosenpath = if chosenPathInd = -1 then [] else List.nth paths chosenPathInd in
+
+    let rd_start, rd_end = match chosenpath with
+      | (c, (s, e))::tl -> e,s
+      | [] -> print_string "RANDOM NUMBERS USED HERE"; (Random.int 53), (Random.int 54) in
+
+(*  
     let rd_start = match roads_of c rl with 
       | (_,(s, e))::tl -> max s e 
       | _ -> Random.int 53 in
-
+    let rd_end = rd_start+1 in
+*)
     let test_path = shortest_path_to 53 c pl [(rd_start, None)] [] [] rl il in
     print_string ((string_of_color c) ^ "\n");
     print_string ("Starting Point : " ^ (string_of_int rd_start));
@@ -65,7 +85,7 @@ module Bot = functor (S : Soul) -> struct
       | ActionRequest -> 
         if is_none t.dicerolled then Action(RollDice) else begin
           match !build_road , !build_town with
-          | true , _ when valid_build_road c pl (c, (rd_start, rd_start+1)) rl il cCOST_ROAD-> build_road:=false; (print_string ("Trying to build a road from" ^(string_of_int rd_start)^ " to " ^ (string_of_int (rd_start+1)))); Action(BuyBuild(BuildRoad(c, (rd_start, rd_start+1))))
+          | true , _ when valid_build_road c pl (c, (rd_start, rd_end)) rl il cCOST_ROAD-> build_road:=false; (print_string ("Trying to build a road from" ^(string_of_int rd_start)^ " to " ^ (string_of_int (rd_end)))); Action(BuyBuild(BuildRoad(c, (rd_start, rd_end))))
           | _, true  when valid_build_town c town_pt pl rl il->  build_town:=false; print_string ("Tring to build town at" ^ (string_of_int town_pt)); Action(BuyBuild(BuildTown(town_pt)))
           | _ -> turn_count:= !turn_count+1; Action(EndTurn)
         end 

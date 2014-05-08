20b805f32440b22af5fc69aa6c72efb7662cf51e
Tai Wei Guo <tg225@cornell.edu>
2014-05-07 20:12:59 -0400

    small changes

--- bot_resources.ml
+++ bot_resources.ml
@@ -225,7 +225,7 @@ let trade_for buildcost history scores inv pl : trade option =
 
 		let players_to_ask = should_ask_for buildcost pl scores in
 		let have_not_asked p : bool =
-			not (List.mem ((color_of p),need,will_pay) history) in
+			not (List.mem ((color_of p),need,will_pay) !history) in
 
 		(* ask a player in pl to give us what we need for what we will pay,
 		   as long as we have not asked this already this turn in history *)
@@ -242,20 +242,20 @@ let trade_for buildcost history scores inv pl : trade option =
 (* takes in a list history that lists the previously requested trades 
    and a hashtable scores that lists the "good trading partner" score 
    for other colors *)
-let handle_trade_initiate history scores cm pl : move option =
+let handle_trade_initiate (history : 'a ref) scores cm pl : move option =
 	let inv = inv_of (player cm pl) in
 	let trade buildcost = trade_for buildcost history scores inv pl in
 	match trade cCOST_CITY with
-	| Some(trd) -> Some (Action(DomesticTrade(trd))); history := trd::!history
+	| Some(trd) -> (history := trd::!history); Some (Action(DomesticTrade(trd)))
 	| None ->
 		match trade cCOST_TOWN with
-		| Some(trd) -> Some (Action(DomesticTrade(trd))); history := trd::!history
+		| Some(trd) -> (history := trd::!history); Some (Action(DomesticTrade(trd)))
 		| None ->
 			match trade cCOST_CARD with
-			| Some(trd) -> Some (Action(DomesticTrade(trd))); history := trd::!history
+			| Some(trd) -> (history := trd::!history); Some (Action(DomesticTrade(trd)))
 			| None -> 
 				match trade cCOST_ROAD with
-					| Some(trd) -> Some (Action(DomesticTrade(trd))); history := trd::!history
+					| Some(trd) -> (history := trd::!history); Some (Action(DomesticTrade(trd)))
 					| None -> None
 
 let should_initiate_trade history scores cm pl : bool =
